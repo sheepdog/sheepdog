32827020c5db0a22b3b9943299465b84e9215562
FUJITA Tomonori <fujita.tomonori@lab.ntt.co.jp>
2010-05-19 01:57:11 +0900

    use gcc built-in ffs()
    Signed-off-by: FUJITA Tomonori <fujita.tomonori@lab.ntt.co.jp>
    Signed-off-by: MORITA Kazutaka <morita.kazutaka@lab.ntt.co.jp>

--- include/bitops.h
+++ include/bitops.h
@@ -3,42 +3,10 @@
 #define BITS_TO_LONGS(nr)	DIV_ROUND_UP(nr, BITS_PER_BYTE * sizeof(long))
 #define DECLARE_BITMAP(name,bits) \
 	unsigned long name[BITS_TO_LONGS(bits)]
-
 #define BITS_PER_LONG (BITS_PER_BYTE * sizeof(long))
 
+#define __ffs(x)  (x ? __builtin_ffsl(x) - 1 : 0)
 #define ffz(x)  __ffs(~(x))
-
-static inline unsigned long __ffs(unsigned long word)
-{
-	int num = 0;
-
-	if (BITS_PER_LONG == 64) {
-		if ((word & 0xffffffff) == 0) {
-			num += 32;
-			word >>= 32;
-		}
-	}
-
-	if ((word & 0xffff) == 0) {
-		num += 16;
-		word >>= 16;
-	}
-	if ((word & 0xff) == 0) {
-		num += 8;
-		word >>= 8;
-	}
-	if ((word & 0xf) == 0) {
-		num += 4;
-		word >>= 4;
-	}
-	if ((word & 0x3) == 0) {
-		num += 2;
-		word >>= 2;
-	}
-	if ((word & 0x1) == 0)
-		num += 1;
-	return num;
-}
 
 #define BITOP_WORD(nr)		((nr) / BITS_PER_LONG)
 
