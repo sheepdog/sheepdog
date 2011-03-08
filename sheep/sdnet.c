66b602a7c060281584f762ce8583d7d0c36191fc
MORITA Kazutaka <morita.kazutaka@lab.ntt.co.jp>
2011-03-09 04:15:30 +0900

    update copyright year to 2011
    Signed-off-by: MORITA Kazutaka <morita.kazutaka@lab.ntt.co.jp>

--- sheep/sdnet.c
+++ sheep/sdnet.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2009-2010 Nippon Telegraph and Telephone Corporation.
+ * Copyright (C) 2009-2011 Nippon Telegraph and Telephone Corporation.
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License version
