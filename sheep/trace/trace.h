e1aefa6ba97b92beb8835051e4ad2c6229b8d76f
Liu Yuan <tailai.ly@taobao.com>
2012-04-10 15:01:58 +0800

    trace: add low level ip2function support
    We use GCC's stabs to map IP to its symbol(Function Name).
    Normally, loader doesn't load symbol into memory, we have to
    manually code ld script to load it.
    
    Signed-off-by: Liu Yuan <tailai.ly@taobao.com>
    Signed-off-by: MORITA Kazutaka <morita.kazutaka@lab.ntt.co.jp>

--- sheep/trace/trace.h
+++ sheep/trace/trace.h
@@ -1,6 +1,20 @@
 #ifndef TRACE_H
 #define TRACE_H
 
+#include "util.h"
+
+struct ipinfo {
+	const char *file;           /* Source code filename for EIP */
+	int line;                   /* Source code linenumber for EIP */
+	const char *fn_name;        /* Name of function containing EIP */
+	int fn_namelen;             /* Length of function name */
+	unsigned long fn_addr;      /* Address of start of function */
+	int fn_narg;                /* Number of function arguments */
+};
+
+/* stabs.c */
+extern int get_ipinfo(unsigned long ip, struct ipinfo *info);
+
 /* mcount.S */
 extern void mcount(void);
 extern void mcount_call(void);
