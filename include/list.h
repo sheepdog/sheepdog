2fdaf4e7a19016bc90fc5a680fbafeaa5cb6e51f
MORITA Kazutaka <morita.kazutaka@lab.ntt.co.jp>
2012-10-06 23:00:44 +0900

    use offsetof in stddef.h
    Current code doesn't use pre-defiend compiler macro for offsetof
    because __compiler_offsetof is not defined anywhere.  This patch
    includes stddef.h, which defines offsetof with __builtin_offsetof,
    rather than redefining it.
    
    Signed-off-by: MORITA Kazutaka <morita.kazutaka@lab.ntt.co.jp>

--- include/list.h
+++ include/list.h
@@ -3,12 +3,7 @@
 
 /* taken from linux kernel */
 
-#undef offsetof
-#ifdef __compiler_offsetof
-#define offsetof(TYPE,MEMBER) __compiler_offsetof(TYPE,MEMBER)
-#else
-#define offsetof(TYPE, MEMBER) ((size_t) &((TYPE *)0)->MEMBER)
-#endif
+#include <stddef.h>
 
 #define container_of(ptr, type, member) ({			\
         const typeof( ((type *)0)->member ) *__mptr = (ptr);	\
