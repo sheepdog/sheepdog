sheepdog/sheepdog

commit 349dab673579e21b009b5677ebc5b80377fca249

sheep: use node_id_cmp to compare node
- remove vnode_node_cmp(), node_cmp()

Signed-off-by: Liu Yuan <tailai.ly@taobao.com>

include/sheep.h
@@ -327,43 +327,26 @@ static inline const char *sd_strerror(int err)
 	return "Invalid error code";
 }
 
-static inline int vnode_node_cmp(const void *a, const void *b)
+static inline int node_id_cmp(const void *a, const void *b)
 {
-	const struct sd_vnode *node1 = a;
-	const struct sd_node *node2 = b;
+	const struct node_id *node1 = a;
+	const struct node_id *node2 = b;
 	int cmp;
 
-	cmp = memcmp(node1->nid.addr, node2->nid.addr, sizeof(node1->nid.addr));
+	cmp = memcmp(node1->addr, node2->addr, sizeof(node1->addr));
 	if (cmp != 0)
 		return cmp;
 
-	if (node1->nid.port < node2->nid.port)
+	if (node1->port < node2->port)
 		return -1;
-	if (node1->nid.port > node2->nid.port)
-		return 1;
-	return 0;
-}
-
-static inline int node_cmp(const void *a, const void *b)
-{
-	const struct sd_node *node1 = a;
-	const struct sd_node *node2 = b;
-	int cmp;
-
-	cmp = memcmp(node1->nid.addr, node2->nid.addr, sizeof(node1->nid.addr));
-	if (cmp != 0)
-		return cmp;
-
-	if (node1->nid.port < node2->nid.port)
-		return -1;
-	if (node1->nid.port > node2->nid.port)
+	if (node1->port > node2->port)
 		return 1;
 	return 0;
 }
 
 static inline int node_eq(const struct sd_node *a, const struct sd_node *b)
 {
-	return node_cmp(a, b) == 0;
+	return node_id_cmp(a, b) == 0;
 }
 
 static inline int vnode_cmp(const void *a, const void *b)

sheep/cluster/accord.c
@@ -266,7 +266,8 @@ static int add_event(struct acrd_handle *ah, enum acrd_event_type type,
 		ev.nr_nodes++;
 		break;
 	case EVENT_LEAVE:
-		n = lfind(node, ev.nodes, &ev.nr_nodes, sizeof(*n), node_cmp);
+		n = lfind(node, ev.nodes, &ev.nr_nodes, sizeof(*n),
+			  node_id_cmp);
 		if (!n)
 			goto out;
 		idx = n - ev.nodes;

sheep/cluster/local.c
@@ -231,7 +231,7 @@ static void add_event(enum local_event_type type, struct sd_node *node,
 		ev.nr_nodes++;
 		break;
 	case EVENT_LEAVE:
-		n = lfind(node, ev.nodes, &ev.nr_nodes, sizeof(*n), node_cmp);
+		n = lfind(node, ev.nodes, &ev.nr_nodes, sizeof(*n), node_id_cmp);
 		if (!n)
 			panic("internal error\n");
 		idx = n - ev.nodes;

sheep/cluster/zookeeper.c
@@ -330,7 +330,7 @@ static inline int zk_node_cmp(const void *a, const void *b)
 {
 	const struct zk_node *znode1 = a;
 	const struct zk_node *znode2 = b;
-	return node_cmp(&znode1->node, &znode2->node);
+	return node_id_cmp(&znode1->node.nid, &znode2->node.nid);
 }
 
 static void node_btree_add(void **btroot, struct zk_node *znode)

sheep/group.c
@@ -125,7 +125,7 @@ bool have_enough_zones(void)
 static int get_node_idx(struct vnode_info *vnode_info, struct sd_node *ent)
 {
 	ent = bsearch(ent, vnode_info->nodes, vnode_info->nr_nodes,
-		      sizeof(*ent), node_cmp);
+		      sizeof(*ent), node_id_cmp);
 	if (!ent)
 		return -1;
 
@@ -217,7 +217,7 @@ static struct vnode_info *alloc_vnode_info(struct sd_node *nodes,
 
 	vnode_info->nr_nodes = nr_nodes;
 	memcpy(vnode_info->nodes, nodes, sizeof(*nodes) * nr_nodes);
-	qsort(vnode_info->nodes, nr_nodes, sizeof(*nodes), node_cmp);
+	qsort(vnode_info->nodes, nr_nodes, sizeof(*nodes), node_id_cmp);
 
 	vnode_info->nr_vnodes = nodes_to_vnodes(nodes, nr_nodes,
 						vnode_info->vnodes);
@@ -574,7 +574,7 @@ static int cluster_wait_for_join_check(struct sd_node *joined,
 			epoch, local_epoch);
 
 		if (bsearch(joined, local_entries, nr_local_entries,
-			    sizeof(struct sd_node), node_cmp))
+			    sizeof(struct sd_node), node_id_cmp))
 			return CJ_RES_FAIL;
 		return CJ_RES_JOIN_LATER;
 	}

sheep/recovery.c
@@ -154,7 +154,7 @@ static int is_invalid_vnode(struct sd_vnode *entry, struct sd_node *nodes,
 				int nr_nodes)
 {
 	if (bsearch(entry, nodes, nr_nodes, sizeof(struct sd_node),
-		    vnode_node_cmp))
+		    node_id_cmp))
 		return 0;
 	return 1;
 }
@@ -532,7 +532,7 @@ static void screen_object_list(struct recovery_work *rw,
 static int newly_joined(struct sd_node *node, struct recovery_work *rw)
 {
 	if (bsearch(node, rw->old_vnodes->nodes, rw->old_vnodes->nr_nodes,
-		    sizeof(struct sd_node), node_cmp))
+		    sizeof(struct sd_node), node_id_cmp))
 		return 0;
 	return 1;
 }

sheep/sockfd_cache.c
@@ -66,23 +66,6 @@ struct sockfd_cache_entry {
 	uint8_t fd_in_use[SOCKFD_CACHE_MAX_FD];
 };
 
-static inline int node_id_cmp(const void *a, const void *b)
-{
-	const struct node_id *node1 = a;
-	const struct node_id *node2 = b;
-	int cmp;
-
-	cmp = memcmp(node1->addr, node2->addr, sizeof(node1->addr));
-	if (cmp != 0)
-		return cmp;
-
-	if (node1->port < node2->port)
-		return -1;
-	if (node1->port > node2->port)
-		return 1;
-	return 0;
-}
-
 static struct sockfd_cache_entry *
 sockfd_cache_insert(struct sockfd_cache_entry *new)
 {

commit 08f5c81c9bcb93bc868e610031c3feacb5d7aded

trivial: pass pointers of node_id for node_id_cmp() in node_eq()
It seems that current node_eq() passes pointers of struct sd_node to
node_id_cmp() as parameters even though node_id_cmp() assumes the
parameters are pointers of node_id.

Currently, it works correctly because of the below 2 reasons:
1. types of parameters of node_id_cmp() are defined as void *.
   It is for qsort()
2. nid of struct node_id is defined as the first member of the struct.

So this might not be a problem until the layout of struct sd_node is
changed. But I think that it is dangerous and should be fixed.

It seems that this problem is caused by a change in the commit
349dab6735.

Cc: Liu Yuan <namei.unix@gmail.com>
Signed-off-by: Hitoshi Mitake <mitake.hitoshi@lab.ntt.co.jp>
Signed-off-by: Liu Yuan <tailai.ly@taobao.com>

include/sheep.h
@@ -250,7 +250,7 @@ static inline int node_id_cmp(const void *a, const void *b)
 
 static inline bool node_eq(const struct sd_node *a, const struct sd_node *b)
 {
-	return node_id_cmp(a, b) == 0;
+	return node_id_cmp(&a->nid, &b->nid) == 0;
 }
 
 static inline int vnode_cmp(const void *a, const void *b)