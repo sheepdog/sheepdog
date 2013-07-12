8324d958cbd48ca1fa49b15d1fcc90ae186faf7c
Kai Zhang <kyle@zelin.io>
2013-07-11 23:41:35 -0700

    tests/unit: add missing copyright and license
    Signed-off-by: Kai Zhang <kyle@zelin.io>
    Signed-off-by: MORITA Kazutaka <morita.kazutaka@lab.ntt.co.jp>

--- tests/unit/sheep/mock_store.c
+++ tests/unit/sheep/mock_store.c
@@ -1,3 +1,16 @@
+/*
+ * Copyright (C) 2013 Zelin.io
+ *
+ * Kai Zhang <kyle@zelin.io>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License version
+ * 2 as published by the Free Software Foundation.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include "sheep_priv.h"
 
 int read_object(uint64_t oid, char *data, unsigned int datalen,
--- tests/unit/sheep/test_vdi.c
+++ tests/unit/sheep/test_vdi.c
@@ -1,3 +1,16 @@
+/*
+ * Copyright (C) 2013 Zelin.io
+ *
+ * Kai Zhang <kyle@zelin.io>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License version
+ * 2 as published by the Free Software Foundation.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include <check.h>
 
 #include "sheep_priv.h"
