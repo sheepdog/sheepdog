b91b0c8e18deeb2c4e886975e348e3b1563505ca
Liu Yuan <namei.unix@gmail.com>
2013-07-31 17:59:12 +0800

    sheep: retire sheep/sockfd_cache.c
    Now the bulk of its sockfd cache code is in the library, so no need to keep
    a dedicated file for it.
    
    Signed-off-by: Liu Yuan <namei.unix@gmail.com>
    Reviewed-by: Hitoshi Mitake <mitake.hitoshi@lab.ntt.co.jp>
    Signed-off-by: MORITA Kazutaka <morita.kazutaka@lab.ntt.co.jp>

--- sheep/request.c
+++ sheep/request.c
@@ -917,3 +917,35 @@ void local_req_init(void)
 		panic("failed to init local req efd");
 	register_event(sys->local_req_efd, local_req_handler, NULL);
 }
+
+int sheep_exec_req(const struct node_id *nid, struct sd_req *hdr, void *buf)
+{
+	struct sd_rsp *rsp = (struct sd_rsp *)hdr;
+	struct sockfd *sfd;
+	int ret;
+
+	assert(is_worker_thread());
+
+	sfd = sockfd_cache_get(nid);
+	if (!sfd)
+		return SD_RES_NETWORK_ERROR;
+
+	ret = exec_req(sfd->fd, hdr, buf, sheep_need_retry, hdr->epoch,
+		       MAX_RETRY_COUNT);
+	if (ret) {
+		sd_dprintf("remote node might have gone away");
+		sockfd_cache_del(nid, sfd);
+		return SD_RES_NETWORK_ERROR;
+	}
+	ret = rsp->result;
+	if (ret != SD_RES_SUCCESS)
+		sd_eprintf("failed %s", sd_strerror(ret));
+
+	sockfd_cache_put(nid, sfd);
+	return ret;
+}
+
+bool sheep_need_retry(uint32_t epoch)
+{
+	return sys_epoch() == epoch;
+}
