bc9074fc61e9074e0de1d8e0398ff04a5ea9e2c3
MORITA Kazutaka <morita.kazutaka@lab.ntt.co.jp>
2013-08-15 11:44:59 +0900

    man: add script/gen_man.pl to generate manuals
    Signed-off-by: MORITA Kazutaka <morita.kazutaka@lab.ntt.co.jp>
    Signed-off-by: Liu Yuan <namei.unix@gmail.com>

---
+++ script/gen_man.pl
@@ -0,0 +1,79 @@
+#!/usr/bin/perl
+#
+# Genrate sheepdog manuals from help messages
+#
+
+use strict;
+
+my ($cwd) = ($0 =~ m%^(.+/)%);
+my $program = $ARGV[0];
+
+## generator functions
+
+sub sheep {
+    my ($line) = @_;
+
+    if ($line =~ /^  ([^,]+), (\S+)\s+(.+)/) {
+	my ($opt, $longopt, $desc) = ($1, $2, $3);
+	print escape(header("$opt, $longopt") . "\n");
+	print escape("$desc\n");
+    }
+}
+
+sub dog {
+    my ($line) = @_;
+
+    if ($line =~ /^  (.+?)  \s+(.+)/) {
+	my ($cmd, $desc) = ($1, $2);
+	my $help = join '', `$program $cmd -h`;
+
+	$help =~ s/Usage: dog (.*)/header($1)/e;
+	$help =~ s/^([A-Z][ a-zA-Z]*:)/\n$1/mg;
+
+	print escape("$help\n");
+	print escape("Description:\n  $desc\n");
+    }
+}
+
+sub sheepfs {
+    my ($line) = @_;
+
+    if ($line =~ /^  ([^,]+), (\S+)\s+(.+)/) {
+	my ($opt, $longopt, $desc) = ($1, $2, $3);
+	print escape(header("$opt, $longopt") . "\n");
+	print escape("$desc\n");
+    }
+}
+
+## helper functions
+
+sub header {
+    my ($str) = @_;
+
+    return ".TP\n.BI \"$str\"";
+}
+
+sub escape {
+    my ($str) = @_;
+
+    $str =~ s/\t/  /g;
+    $str =~ s/\\/\\\\\\/g;
+    $str =~ s/"/\\"/g;
+    $str =~ s/#/\\#/g;
+    $str =~ s/\$/\\\$/g;
+    $str =~ s/\n/\\n/g;
+
+    return $str;
+}
+
+## main routine
+
+open IN, "$program -h |" or die "cannot find $program\n";
+my @help = <IN>;
+close IN;
+
+foreach my $help (@help) {
+    my ($func) = ($program =~ m#.*/(.+)#);
+    chomp($help);
+    eval "$func(\"$help\")";
+}
