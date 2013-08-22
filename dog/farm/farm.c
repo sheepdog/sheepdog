a4d01de7bd31daa7a8e8adfecc5703ef520c7f92
MORITA Kazutaka <morita.kazutaka@lab.ntt.co.jp>
2013-08-22 10:25:54 +0900

    dog: check the result of dog_exec_req() correctly
    dog_exec_req doesn't return a result code after the commit 5940548.
    We have to check rsp->result instead of its return value.
    
    Signed-off-by: MORITA Kazutaka <morita.kazutaka@lab.ntt.co.jp>
    Signed-off-by: Liu Yuan <namei.unix@gmail.com>

--- dog/farm/farm.c
+++ dog/farm/farm.c
@@ -187,7 +187,7 @@ static int notify_vdi_add(uint32_t vdi_id, uint32_t nr_copies)
 
 	ret = dog_exec_req(sdhost, sdport, &hdr, buf);
 
-	if (ret)
+	if (ret < 0)
 		sd_err("Fail to notify vdi add event(%"PRIx32", %d)", vdi_id,
 		       nr_copies);
 
