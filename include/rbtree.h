fcf4308c194061d0edd86b653b74e5d2a3908ec4
Liu Yuan <namei.unix@gmail.com>
2013-09-11 14:24:56 +0800

    rbtree: remove unused augment functions
    We are unlikely to use them in the future. So remove it.
    
    Signed-off-by: Liu Yuan <namei.unix@gmail.com>
    Reviewed-by: Hitoshi Mitake <mitake.hitoshi@lab.ntt.co.jp>
    Signed-off-by: MORITA Kazutaka <morita.kazutaka@lab.ntt.co.jp>

--- include/rbtree.h
+++ include/rbtree.h
@@ -53,14 +53,6 @@ static inline void rb_init_node(struct rb_node *rb)
 void rb_insert_color(struct rb_node *, struct rb_root *);
 void rb_erase(struct rb_node *, struct rb_root *);
 
-typedef void (*rb_augment_f)(struct rb_node *node, void *data);
-
-void rb_augment_insert(struct rb_node *node,
-		rb_augment_f func, void *data);
-struct rb_node *rb_augment_erase_begin(struct rb_node *node);
-void rb_augment_erase_end(struct rb_node *node,
-		rb_augment_f func, void *data);
-
 /* Find logical next and previous nodes in a tree */
 struct rb_node *rb_next(const struct rb_node *);
 struct rb_node *rb_prev(const struct rb_node *);
