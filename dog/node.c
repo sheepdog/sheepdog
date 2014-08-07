325520ce4b3f05e157014fc4d0b065d825e05846
Ruoyu <liangry@ucweb.com>
2014-08-07 12:08:23 +0800

    dog/node: remove checking argument in md plug/unplug since root cause is found
    The purpose of the checking in the function is to ensure the
    argument is not null. But it is not needed any longer because
    the argument should be guaranteed in the framework as long as
    CMD_NEED_ARG is specified in subcommand.
    
    Since the problem is solved in commit e6d1706, we can remove
    the checking safely.
    
    Signed-off-by: Ruoyu <liangry@ucweb.com>
    Signed-off-by: Hitoshi Mitake <mitake.hitoshi@lab.ntt.co.jp>

--- dog/node.c
+++ dog/node.c
@@ -441,14 +441,6 @@ static int do_plug_unplug(char *disks, bool plug)
 	struct sd_rsp *rsp = (struct sd_rsp *)&hdr;
 	int ret;
 
-	if (!disks)
-		return EXIT_USAGE;
-
-	if (!strlen(disks)) {
-		sd_err("Empty path isn't allowed");
-		return EXIT_FAILURE;
-	}
-
 	if (plug)
 		sd_init_req(&hdr, SD_OP_MD_PLUG);
 	else
