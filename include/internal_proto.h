e887c448df8ca5ea32e5883273903599336b9967
Saeki Masaki <saeki.masaki@po.ntts.co.jp>
2014-12-04 18:24:10 +0900

    sheep/dog: move opcode SD_OP_READ_DEL_VDI from sheepdog proto to internal proto
    SD_OP_READ_DEL_VDI is used only dog&sheep communication.
    so move definition SD_OP_READ_DEL_VDI from sheepdog_proto.h to internal_proto.h
    
    [Hitoshi: modified commit log for correct description]
    
    Signed-off-by Masaki Saeki <saeki.masaki@po.ntts.co.jp>
    Signed-off-by: Hitoshi Mitake <mitake.hitoshi@lab.ntt.co.jp>

--- include/internal_proto.h
+++ include/internal_proto.h
@@ -110,6 +110,7 @@
 #define SD_OP_OIDS_EXIST	0xC6
 #define SD_OP_VDI_STATE_SNAPSHOT_CTL  0xC7
 #define SD_OP_INODE_COHERENCE 0xC8
+#define SD_OP_READ_DEL_VDIS  0xC9
 
 /* internal flags for hdr.flags, must be above 0x80 */
 #define SD_FLAG_CMD_RECOVERY 0x0080
