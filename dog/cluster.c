5f292bd53ad7f9a5feee179f8f1483535d0cecbf
Meng Lingkun <menglingkun@cmss.chinamobile.com>
2015-07-22 12:38:48 +0800

    dog: compare copies with zones_nr instead of nodes_nr when format
    Since cluster stores data in different nodes with different zones,
    So fix it.
    
    Signed-off-by: Meng Lingkun <menglingkun@cmss.chinamobile.com>
    Signed-off-by: Hitoshi Mitake <mitake.hitoshi@lab.ntt.co.jp>

--- dog/cluster.c
+++ dog/cluster.c
@@ -87,12 +87,12 @@ static int cluster_format(int argc, char **argv)
 	char store_name[STORE_LEN];
 	static DECLARE_BITMAP(vdi_inuse, SD_NR_VDIS);
 
-	if (cluster_cmd_data.copies > sd_nodes_nr) {
+	if (cluster_cmd_data.copies > sd_zones_nr) {
 		char info[1024];
 		snprintf(info, sizeof(info), "Number of copies (%d) is larger "
-			 "than number of nodes (%d).\n"
+			 "than number of zones (%d).\n"
 			 "Are you sure you want to continue? [yes/no]: ",
-			 cluster_cmd_data.copies, sd_nodes_nr);
+			 cluster_cmd_data.copies, sd_zones_nr);
 		confirm(info);
 	}
 
