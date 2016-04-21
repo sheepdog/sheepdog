84358b27290c630c2b538df23735603b732ce7aa
Takashi Menjo <menjo.takashi@lab.ntt.co.jp>
2016-04-21 16:30:31 +0900

    tests/unit: Replace Check with Unity on sheep/test_vdi.c
    Signed-off-by: Takashi Menjo <menjo.takashi@lab.ntt.co.jp>

--- tests/unit/sheep/test_vdi.c
+++ tests/unit/sheep/test_vdi.c
@@ -1,7 +1,9 @@
 /*
  * Copyright (C) 2013 Zelin.io
+ * Copyright (C) 2016 Nippon Telegraph and Telephone Corporation
  *
  * Kai Zhang <kyle@zelin.io>
+ * Takashi Menjo <menjo.takashi@lab.ntt.co.jp>
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License version
@@ -11,42 +13,24 @@
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
-#include <check.h>
+#include <unity.h>
 
 #include "sheep_priv.h"
 
-START_TEST(test_vdi)
+static void test_vdi(void)
 {
 	struct system_info mock_sys = {0}; sys = &mock_sys;
 	add_vdi_state(1, 1, true, 0, 22, 0);
 	add_vdi_state(2, 1, true, 0, 22, 0);
 	add_vdi_state(3, 2, false, 0, 22, 0);
-
-	ck_assert_int_eq(get_vdi_copy_number(1), 1);
-	ck_assert_int_eq(get_vdi_copy_number(2), 1);
-	ck_assert_int_eq(get_vdi_copy_number(3), 2);
-}
-END_TEST
-
-static Suite *test_suite(void)
-{
-	Suite *s = suite_create("test vdi");
-
-	TCase *tc_vdi = tcase_create("vdi");
-	tcase_add_test(tc_vdi, test_vdi);
-
-	suite_add_tcase(s, tc_vdi);
-
-	return s;
+	TEST_ASSERT_EQUAL_INT(1, get_vdi_copy_number(1));
+	TEST_ASSERT_EQUAL_INT(1, get_vdi_copy_number(2));
+	TEST_ASSERT_EQUAL_INT(2, get_vdi_copy_number(3));
 }
 
 int main(void)
 {
-	int number_failed;
-	Suite *s = test_suite();
-	SRunner *sr = srunner_create(s);
-	srunner_run_all(sr, CK_NORMAL);
-	number_failed = srunner_ntests_failed(sr);
-	srunner_free(sr);
-	return (number_failed == 0) ? EXIT_SUCCESS : EXIT_FAILURE;
+	UNITY_BEGIN();
+	RUN_TEST(test_vdi);
+	return UNITY_END();
 }
